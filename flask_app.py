bdfadc1632dfcc82ddabe2dc86fc95e61e3d8954
techspacebase@gmail.com <gio2001255@gmail.com>
2024-10-13 17:55:19 +0400

    fast

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+def tgbot(self, environ):
+    app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "<h1>Heroku Python Flask Test Page</h1><p>Your Flask app is running successfully on Heroku!</p>"
+    @app.route('/')
+    def index():
+        return "<h1>Heroku Python Flask Test Page</h1><p>Your Flask app is running successfully on Heroku!</p>"
 
-def tgbot():
     return app
